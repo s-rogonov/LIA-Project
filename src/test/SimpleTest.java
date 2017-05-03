9d1be96b735a58acadaff1288df075599fa0054d
Rogonov Stepan <rogonovstepan@gmail.com>
2017-05-03 20:55:23 +0300

    Tests done

---
+++ src/test/SimpleTest.java
@@ -0,0 +1,13 @@
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by sr9000 on 5/3/17.
+ */
+public class SimpleTest {
+    @Test
+    public void test1() throws Exception {
+        assertTrue("It's true!", true);
+    }
+}
