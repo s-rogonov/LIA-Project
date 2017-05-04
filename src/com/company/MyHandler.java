2d490decd60d2b54d04ac1bcc6564e80c90ae831
Rogonov Stepan <rogonovstepan@gmail.com>
2017-05-04 05:27:07 +0300

    post and get

--- src/com/company/MyHandler.java
+++ src/com/company/MyHandler.java
@@ -4,13 +4,25 @@ import com.sun.net.httpserver.HttpHandler;
 
 import java.io.IOException;
 import java.io.OutputStream;
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Created by sr9000 on 5/4/17.
  */
 public class MyHandler implements HttpHandler {
+    List<String> myList = new ArrayList<String>();
     @Override
     public void handle(HttpExchange t) throws IOException {
-        String response = "This is the response";
+        String response = org.apache.commons.io.IOUtils.toString(t.getRequestBody());
+        if (t.getRequestMethod().equalsIgnoreCase("get")) {
+            response = "This is the response";
+        }
+
+        response = "<html><body><p>"
+                + response
+                + "</p><form method=`post`>First name:<br><input type=`text` name=`firstname`><br>Last name:<br><input type=`text` name=`lastname`><br><input type=`submit`></form></body></html>".replace('`', '"');
+
         t.sendResponseHeaders(200, response.length());
         OutputStream os = t.getResponseBody();
         os.write(response.getBytes());
